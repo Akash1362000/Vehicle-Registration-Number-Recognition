b58c2a08cc788e7ee871fd4b32e8d5ac446c767a
Akash Shrivastava <akash.shrivastava136@gmail.com>
2023-09-19 12:22:14 +0530

    test: Add testcases for reg no detection

--- plate_detector/plate_detector/settings.py
+++ plate_detector/plate_detector/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
--- plate_detector/registration/tests.py
+++ plate_detector/registration/tests.py
@@ -1,3 +1,29 @@
+from django.contrib.staticfiles import finders
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import VehicleImage
+from .service import process_image
+
+
+class VehicleImageTestCase(TestCase):
+    def setUp(self):
+        self.image_path = finders.find("car.jpg")
+
+        with open(self.image_path, "rb") as infile:
+            self.test_image = SimpleUploadedFile(self.image_path, infile.read())
+
+    def test_process_image(self):
+        result = process_image(image_path=self.image_path)
+        self.assertEqual(result, "786P0")
+
+    def test_upload_image_view(self):
+        response = self.client.post(reverse("upload_image"), {"image": self.test_image})
+        self.assertEqual(response.status_code, 302)
+        uploaded_image = VehicleImage.objects.first()
+        self.test_process_image()
+
+    def test_image_list_view(self):
+        response = self.client.get(reverse("image_list"))
+        self.assertEqual(response.status_code, 200)
